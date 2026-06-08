8b6f9a57f96c4d0e8d6742530f8dd8078cd0588b
divyam234 <47589864+divyam234@users.noreply.github.com>
2026-06-08 15:45:18 +0530

    Add lazyvim language extras (python, go, typescript, nix)

--- modules/aspects/neovim/default.nix
+++ modules/aspects/neovim/default.nix
@@ -7,13 +7,25 @@
         imports = [
           inputs.lazyvim.homeManagerModules.default
         ];
-
         programs.lazyvim = {
           enable = true;
-          extras.lang.nix.enable = true;
+          extras = {
+            lang.nix.enable = true;
+            lang.python = {
+              enable = true;
+              installDependencies = true;
+            };
+            lang.go = {
+              enable = true;
+              installDependencies = true;
+            };
+            lang.typescript = {
+              enable = true;
+              installDependencies = false;
+            };
+          };
           extraPackages = with pkgs; [
-            lua-language-server
-            stylua
+            alejandra
           ];
         };
       };
