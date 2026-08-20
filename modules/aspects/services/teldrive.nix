cf661d4dbbf470065d8c4c4b21ab5c4b299ab2c0
Divyam <47589864+divyam234@users.noreply.github.com>
2026-08-20 22:35:38 +0530

    feat: add mtproxy to teldrive

--- modules/aspects/services/teldrive.nix
+++ modules/aspects/services/teldrive.nix
@@ -30,12 +30,12 @@
             TELDRIVE_SECURITY_DATA_KEY=${secrets.teldrive.data_key}
             TELDRIVE_ENCRYPTION_ACTIVE_KEY_VERSION=1
             TELDRIVE_ENCRYPTION_KEYS=1:${secrets.teldrive.encryption_key}
-            TELDRIVE_TELEGRAM_PROXY=http://gluetun:3128
+            TELDRIVE_TELEGRAM_MTPROXY_ADDRESS=mtproxy:443
+            TELDRIVE_TELEGRAM_MTPROXY_SECRET=${secrets.mtproxy.secret}
             TELDRIVE_DATABASE_AUTO_MIGRATE_LEGACY=false
             TELDRIVE_TELEGRAM_DOWNLOAD_CLIENT_POOL=true
             TELDRIVE_TELEGRAM_DOWNLOAD_BOTS=4
-            TELDRIVE_TELEGRAM_RATE_INTERVAL=50ms
-            TELDRIVE_TELEGRAM_RATE_BURST=20
+            TELDRIVE_TELEGRAM_RATE_LIMIT=false
             TELDRIVE_SECURITY_ACCESS_TOKEN_TTL=24h
             TELDRIVE_SECURITY_REFRESH_TOKEN_TTL=8760h
           '';
@@ -54,12 +54,12 @@
           unitConfig = {
             After = [
               quadlet.containers.postgres.ref
-              quadlet.containers.gluetun.ref
+              quadlet.containers.mtproxy.ref
               "postgres-provision.service"
             ];
             Requires = [
               quadlet.containers.postgres.ref
-              quadlet.containers.gluetun.ref
+              quadlet.containers.mtproxy.ref
               "postgres-provision.service"
             ];
           };
