37745f263fa762a8d152345097756c858aca9107
Divyam <47589864+divyam234@users.noreply.github.com>
2026-09-12 19:04:14 +0530

    feat: enable BBR congestion control

--- modules/aspects/features/common.nix
+++ modules/aspects/features/common.nix
@@ -11,6 +11,12 @@
     ];
 
     nixos = { pkgs, ... }: {
+      boot.kernelModules = [ "tcp_bbr" ];
+      boot.kernel.sysctl = {
+        "net.core.default_qdisc" = "fq";
+        "net.ipv4.tcp_congestion_control" = "bbr";
+      };
+
       environment.systemPackages = with pkgs; [
         parted
         efibootmgr
